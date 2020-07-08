eda7edaf4752b2d58696348a4e87fd3b6900d457
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2020-07-08 07:37:55 -0400

    Commit!

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const si = require('systeminformation')
 const colors = require('colors')
 const watch = require('watch-bash')
 
+const args = process.argv.slice(2)
+
 setInterval(() => {
     si.cpuTemperature((data) => {
         var string = `Main`.blue + `: ${data.main}° C - ` + `Max`.yellow + `: ${data.max}° C`
